87f13bb74f35ed2ca8d8c4d2599943c3d8a4e4f8
AmyShull <104698505+AmyShull@users.noreply.github.com>
2022-06-02 10:38:54 -0400

    Update chore.js

--- chore.js
+++ chore.js
@@ -39,6 +39,7 @@ document.querySelector('#minutes').innerHTML = currentMinute;
 
 
 const choreWeek = document.querySelector('#choreList');
+const area = document.querySelector('#job');
 
 //Chorelists
 let Chorelists = {
@@ -85,93 +86,74 @@ const sortBy = () => {
   let job = [];
   switch (document.querySelector('#sortBy').value) {
     case 'Meredith':
-      switch (d.getDate()) {
-        case 1, 2, 3, 4, 5, 6, 7:
+      if (d > 0 && d <= 7){
           job = Chorelists.bathrooms;
-          break;
-        case 8, 9, 10, 11, 12, 13, 14:
+          area = 'Bathrooms';
+      }else if (d >= 8 && d <= 14){
           job = Chorelists.firstFloor;
-          break;
-        case 15, 16, 17, 18, 19, 20, 21:
+          area = 'First Floor';
+      }else if (d >= 15 && d <= 21){
           job = Chorelists.kitchenLaundry;
-          break;
-        case 22, 23, 24, 25, 26, 27, 28, 29, 30, 31:
+          area = 'Kitchen and Laundry';
+      }else if (d >= 22 && d <= 31){
           job = Chorelists.basementFloor;
-          break;
+          area = 'Basement Floor';
       }
-      break;
+    break;
     case 'Genevieve':
-      switch (d.getDate()) {
-        case 1,2,3,4,5,6,7:
-          job = Chorelists.firstFloor;
-          break;
-        case 8,9,10,11,12,13,14:
-          job = Chorelists.kitchenLaundry;
-          break;
-        case 15,16,17,18,19,20,21:
-          job = Chorelists.basementFloor;
-          break;
-        case 22,23,24,25,26,27,28,29,30,31:
-          job = Chorelists.bathrooms;
-          break;
-      }
-      break;
+      if (d > 0 && d <= 7){
+        job = Chorelists.firstFloor;
+        area = 'First Floor';
+    }else if (d >= 8 && d <= 14){
+      job = Chorelists.kitchenLaundry;
+      area = 'Kitchen and Laundry';
+    }else if (d >= 15 && d <= 21){
+      job = Chorelists.basementFloor;
+      area = 'Basement Floor';
+    }else if (d >= 22 && d <= 31){
+      job = Chorelists.bathrooms;
+      area = 'Bathrooms';  
+    }
+    break;
     case 'Elliott':
-      switch (d.getDate()) {
-        case 1,2,3,4,5,6,7:
-          job = Chorelists.kitchenLaundry;
-          break;
-        case 8,9,10,11,12,13,14:
-          job = Chorelists.basementFloor;
-          break;
-        case 15,16,17,18,19,20,21:
-          job = Chorelists.bathrooms;
-          break;
-        case 22,23,24,25,26,27,28,29,30,31:
-          job = Chorelists.firstFloor;
-          break;
+      if (d > 0 && d <= 7){
+        job = Chorelists.kitchenLaundry;
+        area = 'Kitchen and Laundry';
+      }else if (d >= 8 && d <= 14){
+        job = Chorelists.basementFloor;
+        area = 'Basement Floor';
+      }else if (d >= 15 && d <= 21){
+        job = Chorelists.bathrooms;
+        area = 'Bathrooms';
+      }else if (d >= 22 && d <= 31){
+        job = Chorelists.firstFloor;
+        area = 'First Floor';  
       }
-      break;
+    break;
     case 'Amylynne':
-      switch (d.getDate()) {
-        case 1,2,3,4,5,6,7:
-          job = Chorelists.basementFloor;
-          break;
-        case 8,9,10,11,12,13,14: 
-          job = Chorelists.bathrooms;
-          break;
-        case 15,16,17,18,19,20,21: 
-          job = Chorelists.firstFloor;
-          break;
-        case 22,23,24,25,26,27,28,29,30,31: 
-          job = Chorelists.kitchenLaundry;
-          break;
+      if (d > 0 && d <= 7){
+        job = Chorelists.basementFloor;
+        area = 'Basement Floor';
+      }else if (d >= 8 && d <= 14){
+        job = Chorelists.bathrooms;
+        area = 'Bathrooms';
+      }else if (d >= 15 && d <= 21){
+        job = Chorelists.firstFloor;
+        area = 'First Floor';  
+      }else if (d >= 22 && d <= 31){
+        job = Chorelists.kitchenLaundry;
+        area = 'Kitchen and Laundry';
       }
-      break;
-
+    break;
   }
   buildJobList(job);
 };
 
-if (job = Chorelists.bathrooms) {
-  area = 'Bathrooms';
-}else if (job = Chorelists.firstFloor){
-  area = 'First Floor';
-}else if (job = Chorelists.kitchenLaundry){
-  area = 'Kitchen and Laundry';
-}else if (job = Chorelists.basementFloor){
-  area = 'Basement Floor';
-}
-
-
 function buildJobList(job) {
-  //document.querySelector('#name').innerHTML = document.querySelector('#sortBy').value;
-  //document.querySelector('#job').innerHTML = ;
-
   job.forEach((chore) => {
-    li = document.createElement('li');
-    li.textContent = chore;
-    choreWeek.appendChild(li);
+  li = document.createElement('li');
+  li.textContent = chore;
+  choreWeek.appendChild(li);
   });
 }
 
